9d7f17314f06eb54998edda68abb1fc9d00a5b79
Tomas Kelly <to.kelly@f5.com>
2023-12-08 08:49:41 +0000

    ci: turn on local web server for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,9 +20,9 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
   ],
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
